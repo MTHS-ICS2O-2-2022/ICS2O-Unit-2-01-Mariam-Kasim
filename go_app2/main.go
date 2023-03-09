ce9dcc1b636f76c22f2479e37c0987d4bf823327
Patrick Coxall <mr.coxall@mths.ca>
2023-03-09 18:50:05 +0000

    test commit

---
+++ go_app2/main.go
@@ -0,0 +1,15 @@
+// Created by: Mariam Kasim
+// Created on: Feb 2023
+//
+// This program displays, "Hello, World!"
+
+package main
+
+import "fmt"
+
+func main() {
+	// This program displays, "Hello, World!"
+	fmt.Println("Hello, World!")
+
+	fmt.Println("\nDone.")
+}
